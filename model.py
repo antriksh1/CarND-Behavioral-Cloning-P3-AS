b9e5832c3a6e0069f6e21f7713dd630283f312d7
Antriksh Sharma <asharma@placeiq.com>
2018-04-25 21:47:27 -0400

    Updated model.py with comments and some writeup done

--- model.py
+++ model.py
@@ -6,8 +6,10 @@ import numpy as np
 import sklearn
 import gc
 
+# Training folder that I used to train - combination of provided 'data' and my training set
 training_folder = 'data_training3'
 
+# Reading in the log
 samples = []
 with open(training_folder + '/driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
@@ -16,8 +18,11 @@ with open(training_folder + '/driving_log.csv') as csvfile:
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+
+# training - validation split of 70% - 30%
 train_samples, validation_samples = train_test_split(samples, test_size=0.3)
 
+# Generator to provide the data to the model
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:
@@ -53,6 +58,9 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+# After trying the NVIDIA model + another model I found on the web, and 2 variations of LeNet, 
+# this is the modified version of LeNet I chose, because it gave me the smoothest driving
+
 # LeNet
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
@@ -68,9 +76,11 @@ model.add(Dense(80))
 model.add(Dense(40))
 model.add(Dense(1))
 
+# Generating and saving the model
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch=6*len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=8)
 model.save('model.h5.' + training_folder)
 print("Model Saved!")
 
+# Garbage-Collection - to prevent memory leaks
 gc.collect()
